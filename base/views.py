2c83590c37b7d635b40600858b0270b1466c2873
ReeseRuffin <Reese.Ruffin@Genesys.com>
2023-03-13 14:53:58 -0400

    most of the models are complete. going to do CSS later

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -9,3 +12,28 @@ class TaskList(ListView):
     model = Task
     # Giving it a name to make it easy to use in code blocks
     context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    # Using this method to re-route using to the task page
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    # Using this method to re-route using to the task page
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
